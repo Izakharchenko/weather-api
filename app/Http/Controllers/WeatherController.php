0249654315405ecd3539c9b9d898563e582ca3ab
Ivan Zakharchenko <ipzakharchenko@gamil.com>
2024-05-28 17:34:02 +0300

    add validation to weather request

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\WeatherRequest;
 use App\Http\Resources\WeatherResource;
 use App\Models\Weather;
 use Illuminate\Http\Request;
@@ -21,14 +22,16 @@ class WeatherController extends Controller
     /**
      * Get weather data for specific city within a date range.
      */
-    public function getByCityAndDateRange(Request $request, $city_id)
+    public function getByCityAndDateRange(WeatherRequest $request, $city_id)
     {
-        $start_date = $request->input('start_date');
-        $end_date = $request->input('end_date');
+        $validated = $request->validated();
+
+        $start_date = $validated['start_date'];
+        $end_date = $validated['end_date'];
 
         $weather_data = Weather::where('city_id', $city_id)
-                                ->whereBetween('date', [$start_date, $end_date])
-                                ->get();
+            ->whereBetween('date', [$start_date, $end_date])
+            ->get();
 
         return WeatherResource::collection($weather_data);
     }
